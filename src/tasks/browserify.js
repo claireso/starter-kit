6bb086465758948bce46820c80c484480083efc3
Claire Sosset <claire.sosset@gmail.com>
2015-02-11 21:15:25 +0100

    adding error notifications

--- src/tasks/browserify.js
+++ src/tasks/browserify.js
@@ -1,11 +1,15 @@
-var gulp = require('gulp'),
+var gulp       = require('gulp'),
     browserify = require('browserify'),
-    paths = require('../config.js'),
-    source = require('vinyl-source-stream');
+    paths      = require('../config.js'),
+    source     = require('vinyl-source-stream')
+    notify     = require("gulp-notify");
 
 gulp.task('browserify', ['cleanScripts'], function() {
     return browserify('./' + paths.scripts + 'base.js')
         .bundle()
+        .on('error', notify.onError(function (error) {
+            return "Js: " + error.message;
+        }))
         .pipe(source('app.js'))
         .pipe(gulp.dest(paths.dist + paths.scripts));
 });
--- src/tasks/styles.js
+++ src/tasks/styles.js
@@ -1,9 +1,13 @@
-var gulp = require('gulp'),
+var gulp   = require('gulp'),
     stylus = require('gulp-stylus'),
-    paths = require('../config.js');
+    paths  = require('../config.js'),
+    notify = require("gulp-notify");
 
 gulp.task('styles', ['cleanStyles'], function() {
     return gulp.src(paths.styles + 'styles.styl')
     .pipe(stylus())
+    .on('error', notify.onError(function (error) {
+        return "Stylus: " + error.message;
+    }))
     .pipe(gulp.dest(paths.dist + paths.styles));
 });
